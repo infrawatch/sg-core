21d1c2498d1f6a81f3aca68be4bcaefd2007ff16
pleimer <41337120+pleimer@users.noreply.github.com>
2021-03-02 08:20:53 -0500

    add configuration for deleting indices on startup (#26)

--- plugins/application/elasticsearch/pkg/lib/config.go
+++ plugins/application/elasticsearch/pkg/lib/config.go
@@ -2,15 +2,16 @@ package lib
 
 //AppConfig holds configuration for Elasticsearch client
 type AppConfig struct {
-	HostURL       string `yaml:"hostURL"`
-	UseTLS        bool   `yaml:"useTLS"`
-	TLSServerName string `yaml:"tlsServerName"`
-	TLSClientCert string `yaml:"tlsClientCert"`
-	TLSClientKey  string `yaml:"tlsClientKey"`
-	TLSCaCert     string `yaml:"tlsCaCert"`
-	UseBasicAuth  bool   `yaml:"useBasicAuth"`
-	User          string `yaml:"user"`
-	Password      string `yaml:"password"`
-	BufferSize    int    `yaml:"bufferSize"`
-	BulkIndex     bool   `yaml:"bulkIndex"`
+	HostURL       string   `yaml:"hostURL"`
+	UseTLS        bool     `yaml:"useTLS"`
+	TLSServerName string   `yaml:"tlsServerName"`
+	TLSClientCert string   `yaml:"tlsClientCert"`
+	TLSClientKey  string   `yaml:"tlsClientKey"`
+	TLSCaCert     string   `yaml:"tlsCaCert"`
+	UseBasicAuth  bool     `yaml:"useBasicAuth"`
+	User          string   `yaml:"user"`
+	Password      string   `yaml:"password"`
+	BufferSize    int      `yaml:"bufferSize"`
+	BulkIndex     bool     `yaml:"bulkIndex"`
+	ResetIndices  []string `yaml:"resetIndices"`
 }
