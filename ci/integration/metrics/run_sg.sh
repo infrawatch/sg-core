761f7be8311087df25a7a5b21d90a0906c07fabd
Martin Mágr <mmagr@redhat.com>
2021-12-22 11:31:52 +0100

    Cover metrics part with integration tests
    This patch tests following paths:
    
     - Ceilometer > QDR > sg-core > Prometheus
     - collectd > QDR > sg-bridge > sg-core > Prometheus

---
+++ ci/integration/metrics/run_sg.sh
@@ -0,0 +1,29 @@
+#!/bin/env bash
+# CI script for UBI8 job
+# purpose: spawn sg-core to process messages sent by rsyslog
+
+set -ex
+
+# enable required repo(s)
+cat > /etc/yum.repos.d/fedora-eln.repo <<EOF
+[centos-opstools]
+name=opstools
+baseurl=http://mirror.centos.org/centos/8/opstools/\$basearch/collectd-5/
+gpgcheck=0
+enabled=1
+module_hotfixes=1
+EOF
+
+dnf install -y git golang gcc make qpid-proton-c-devel
+
+export GOBIN=$GOPATH/bin
+export PATH=$PATH:$GOBIN
+
+go install golang.org/dl/go1.15@latest
+go1.15 download
+
+# install sg-core and start sg-core
+mkdir -p /usr/lib64/sg-core
+PLUGIN_DIR=/usr/lib64/sg-core/ GOCMD=go1.15 ./build.sh
+
+./sg-core -config ./ci/integration/metrics/sg_config.yaml
---
+++ ci/integration/metrics/run_validation.sh
@@ -0,0 +1,43 @@
+#!/bin/env bash
+# CI script for UBI8 job
+# purpose: verify the expected metric data is scraped by Prometheus
+
+set -ex
+
+dnf install -y jq hostname
+
+PROMETHEUS_URL=http://127.0.0.1:9090
+METRICS=$(curl -s "$PROMETHEUS_URL/api/v1/label/__name__/values"  | jq -r .data)
+
+######################### gather ceilometer data #########################
+ceilo_found=""
+for item in $METRICS; do
+  if [[ $item == \"ceilometer_* ]]; then
+    if [[ -z "$ceilo_found" ]]; then
+      ceilo_found=$item
+    else
+      ceilo_found="$ceilo_found, $item"
+    fi
+  fi
+done
+
+######################### gather collectd data #########################
+collectd_found=""
+for item in $METRICS; do
+  if [[ $item == \"collectd_* ]]; then
+    if [[ -z "$collectd_found" ]]; then
+      collectd_found=$item
+    else
+      collectd_found="$collectd_found, $item"
+    fi
+  fi
+done
+
+############################### validate ###############################
+echo "Ceilometer metrics stored: $ceilo_found"
+echo "Collectd metrics stored: $collectd_found"
+
+if [[ -z "$ceilo_found" ]] || [[ -z "$collectd_found" ]]; then
+  echo "Missing expected metrics data"
+  exit 1
+fi
