8bbaf66811582626c0b4b9d5c6b15c16654ee834
Jaromír Wysoglad <jwysogla@redhat.com>
2022-11-28 15:49:19 +0100

    Add TCP socket tests

--- plugins/transport/socket/main_test.go
+++ plugins/transport/socket/main_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/binary"
 	"io/ioutil"
 	"net"
 	"os"
@@ -132,5 +134,101 @@ func TestUdpSocketTransport(t *testing.T) {
 		cancel()
 		wg.Wait()
 		wskt.Close()
+	})
+}
+
+func TestTcpSocketTransport(t *testing.T) {
+	tmpdir, err := ioutil.TempDir(".", "socket_test_tmp")
+	require.NoError(t, err)
+	defer os.RemoveAll(tmpdir)
+
+	logpath := path.Join(tmpdir, "test.log")
+	logger, err := logging.NewLogger(logging.DEBUG, logpath)
+	require.NoError(t, err)
+
+	trans := Socket{
+		conf: configT{
+			Socketaddr: "127.0.0.1:8642",
+			Type:       "tcp",
+		},
+		logger: &logWrapper{
+			l: logger,
+		},
+	}
+
+	t.Run("test large message transport single connection", func(t *testing.T) {
+		msg := make([]byte, regularBuffSize)
+		addition := "wubba lubba dub dub"
+		for i := 0; i < regularBuffSize; i++ {
+			msg[i] = byte('X')
+		}
+		msg[regularBuffSize-1] = byte('$')
+		msg = append(msg, []byte(addition)...)
+		msg_length := new(bytes.Buffer)
+		err := binary.Write(msg_length, binary.LittleEndian, len(msg))
+		msg = append(msg_length.Bytes(), msg...)
+
+		// verify transport
+		ctx, cancel := context.WithCancel(context.Background())
+		wg := sync.WaitGroup{}
+		go trans.Run(ctx, func(mess []byte) {
+			wg.Add(1)
+			strmsg := string(mess)
+			assert.Equal(t, regularBuffSize+len(addition), len(strmsg))   // we received whole message
+			assert.Equal(t, addition, strmsg[len(strmsg)-len(addition):]) // and the out-of-band part is correct
+			wg.Done()
+		}, make(chan bool))
+		time.Sleep(2 * time.Second)
+
+		// write to socket
+		wskt, err := net.Dial("tcp", "127.0.0.1:8642")
+		require.NoError(t, err)
+		_, err = wskt.Write(msg)
+		require.NoError(t, err)
+
+		cancel()
+		wg.Wait()
+		wskt.Close()
+	})
+
+	t.Run("test large message transport multiple connections", func(t *testing.T) {
+		msg := make([]byte, regularBuffSize)
+		addition := "wubba lubba dub dub"
+		for i := 0; i < regularBuffSize; i++ {
+			msg[i] = byte('X')
+		}
+		msg[regularBuffSize-1] = byte('$')
+		msg = append(msg, []byte(addition)...)
+		msg_length := new(bytes.Buffer)
+		err := binary.Write(msg_length, binary.LittleEndian, len(msg))
+		msg = append(msg_length.Bytes(), msg...)
+
+		// verify transport
+		ctx, cancel := context.WithCancel(context.Background())
+		wg := sync.WaitGroup{}
+		go trans.Run(ctx, func(mess []byte) {
+			wg.Add(1)
+			strmsg := string(mess)
+			assert.Equal(t, regularBuffSize+len(addition), len(strmsg))   // we received whole message
+			assert.Equal(t, addition, strmsg[len(strmsg)-len(addition):]) // and the out-of-band part is correct
+			wg.Done()
+		}, make(chan bool))
+		time.Sleep(2 * time.Second)
+
+		// write to socket
+		wskt1, err := net.Dial("tcp", "127.0.0.1:8642")
+		require.NoError(t, err)
+		wskt2, err := net.Dial("tcp", "127.0.0.1:8642")
+		require.NoError(t, err)
+
+		_, err = wskt1.Write(msg)
+		require.NoError(t, err)
+		_, err = wskt2.Write(msg)
+		require.NoError(t, err)
+
+		cancel()
+		wg.Wait()
+		wskt1.Close()
+		wskt2.Close()
 	})
 }
