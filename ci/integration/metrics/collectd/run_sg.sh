b5fa49485ec3674a303cdc5495ad87077a827de5
Chris Sibbitt <csibbitt@redhat.com>
2023-12-14 12:37:24 -0500

    Move to golang 1.20

--- ci/integration/metrics/collectd/run_sg.sh
+++ ci/integration/metrics/collectd/run_sg.sh
@@ -13,11 +13,11 @@ dnf install -y git golang gcc make qpid-proton-c-devel
 export GOBIN=$GOPATH/bin
 export PATH=$PATH:$GOBIN
 
-go install golang.org/dl/go1.19@latest
-go1.19 download
+go install golang.org/dl/go1.20@latest
+go1.20 download
 
 # install sg-core and start sg-core
 mkdir -p /usr/lib64/sg-core
-PLUGIN_DIR=/usr/lib64/sg-core/ GOCMD=go1.19 BUILD_ARGS=-buildvcs=false ./build.sh
+PLUGIN_DIR=/usr/lib64/sg-core/ GOCMD=go1.20 BUILD_ARGS=-buildvcs=false ./build.sh
 
 ./sg-core -config ./ci/integration/metrics/collectd/sg_config.yaml
