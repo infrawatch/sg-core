35d6fbeac82fc3313f8d73bca28cf5fc6f18d5cf
Chris Sibbitt <csibbitt@redhat.com>
2023-12-19 05:35:21 -0500

    Move to golang 1.20 (#127)
    * Move to golang 1.20
    
    * Bump golangci-lint to 1.51 for golang 1.20 support

--- ci/unit/run_tests.sh
+++ ci/unit/run_tests.sh
@@ -14,8 +14,8 @@ yum install -y git golang gcc make glibc-langpack-en qpid-proton-c-devel
 export GOBIN=$GOPATH/bin
 export PATH=$PATH:$GOBIN
 
-go install golang.org/dl/go1.19@latest
-go1.19 download
+go install golang.org/dl/go1.20@latest
+go1.20 download
 
 
-go1.19 test -v -coverprofile=profile.cov ./...
+go1.20 test -v -coverprofile=profile.cov ./...
