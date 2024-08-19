bd75aa6737cb722a322431f0627a5966e0822c05
vimartin@redhat.com <victoria@redhat.com>
2024-08-19 10:39:07 +0200

    Update golang version in CI/integration scripts
    Add z version when retrieving golang binaries

--- ci/integration/metrics/ceilometer/tcp/run_sg.sh
+++ ci/integration/metrics/ceilometer/tcp/run_sg.sh
@@ -1,23 +1,22 @@
 #!/bin/env bash
-# CI script for UBI8 job
+# CI script for UBI9 job
 # purpose: spawn sg-core to process messages sent by rsyslog
 
 set -ex
 
 # enable required repo(s)
 curl -o /etc/yum.repos.d/CentOS-OpsTools.repo $OPSTOOLS_REPO
-sed -i 's/gpgcheck=1/gpgcheck=0/g' /etc/yum.repos.d/CentOS-OpsTools.repo
 
 dnf install -y git golang gcc make qpid-proton-c-devel
 
 export GOBIN=$GOPATH/bin
 export PATH=$PATH:$GOBIN
 
-go install golang.org/dl/go1.22@latest
-go1.22 download
+go install golang.org/dl/go1.22.0@latest
+go1.22.0 download
 
 # install sg-core and start sg-core
 mkdir -p /usr/lib64/sg-core
-PLUGIN_DIR=/usr/lib64/sg-core/ GOCMD=go1.22 BUILD_ARGS=-buildvcs=false ./build.sh
+PLUGIN_DIR=/usr/lib64/sg-core/ GOCMD=go1.22.0 BUILD_ARGS=-buildvcs=false ./build.sh
 
 ./sg-core -config ./ci/integration/metrics/ceilometer/tcp/sg_config.yaml
