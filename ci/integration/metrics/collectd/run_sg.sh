368bd2b74c6dde78bddfcb44bac7720a2c8891fa
Victoria Martinez de la Cruz <victoria@redhat.com>
2026-04-14 16:16:34 +0200

    Update to golang 1.25.9
    Closes-Bug: https://redhat.atlassian.net/browse/OSPRH-28974
    Closes-Bug: https://redhat.atlassian.net/browse/OSPRH-27803

--- ci/integration/metrics/collectd/run_sg.sh
+++ ci/integration/metrics/collectd/run_sg.sh
@@ -12,11 +12,11 @@ dnf install -y git golang gcc make qpid-proton-c-devel
 export GOBIN=$GOPATH/bin
 export PATH=$PATH:$GOBIN
 
-go install golang.org/dl/go1.24.11@latest
-go1.24.11 download
+go install golang.org/dl/go1.25.9@latest
+go1.25.9 download
 
 # install sg-core and start sg-core
 mkdir -p /usr/lib64/sg-core
-PLUGIN_DIR=/usr/lib64/sg-core/ GOCMD=go1.24.11 BUILD_ARGS=-buildvcs=false ./build.sh
+PLUGIN_DIR=/usr/lib64/sg-core/ GOCMD=go1.25.9 BUILD_ARGS=-buildvcs=false ./build.sh
 
 ./sg-core -config ./ci/integration/metrics/collectd/sg_config.yaml
